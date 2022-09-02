9f927e6fc2357b0946edd114d46204e9a8515e0a
yeon <choyeon960325@gmail.com>
2022-09-02 10:19:28 +0900

    [수정] QnABoard Repository 코드 변경 및 Test 진행

--- BackQnA/src/main/java/com/golflearn/domain/repository/QnABoardRepository.java
+++ BackQnA/src/main/java/com/golflearn/domain/repository/QnABoardRepository.java
@@ -1,9 +1,59 @@
 package com.golflearn.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.golflearn.domain.entity.QnABoard;
+import com.golflearn.domain.entity.QnABoardEntity;
 
-public interface QnABoardRepository extends JpaRepository<QnABoard, Long> {
-
+public interface QnABoardRepository extends JpaRepository <QnABoardEntity, Long> {
+//	List<QnABoardEntity> findByAll(org.springframework.data.domain.Pageable paging);
+	//목록 불러오기
+	@Query(value ="SELECT *\r\n"
+			+"	   FROM(\r\n"
+			+" 	   	     SELECT rownum r, a.*\r\n"
+			+"    		 FROM (SELECT qna_board_no, qna_board_title, qna_board_dt, user_nickname\r\n"
+			+" 				   FROM qna_board \r\n"
+			+" 				   ORDER BY qna_board_no DESC\r\n"
+			+" 					) a\r\n"
+			+" 			  ) \r\n"
+			+"      WHERE r BETWEEN ?1 AND ?2"
+			, nativeQuery=true)
+	List<QnABoardEntity> findByPage(int startRow, int endRow);
+	
+	//공개글인것만 불러오기(qna_board_secret이 0인것)
+	@Query(value = "SELECT *\r\n"
+			+ "		FROM( SELECT rownum r, a.*\r\n"
+			+ "     	   FROM (\r\n"
+			+ "           		 SELECT qna_board_no, qna_board_title, qna_board_dt, user_nickname\r\n"
+			+ "            		 FROM qna_board  \r\n"
+			+ "            		 WHERE qna_board_secret ='0' \r\n"
+			+ "            		 ORDER BY qna_board_dt DESC\r\n"
+			+ "            		)a\r\n"
+			+ "   		 )\r\n"
+			+ "		WHERE r BETWEEN ?1 AND ?2"
+			, nativeQuery=true)
+	List<QnABoardEntity> findByOpenPost(int startRow, int endRow);
+	
+	//상세보기
+	@Query(value = "SELECT qb.*, qc.*\r\n"
+			+ "   FROM qna_board qb LEFT JOIN qna_comment qc\r\n"
+			+ "   ON(qb.qna_board_no = qc.qna_cmt_no)\r\n"
+			+ "   WHERE qb.qna_board_no = ?1"
+			, nativeQuery=true)
+	List<QnABoardEntity> findDetail(Long qnaBoardNo);
+	
+	//작성자로 검색하기
+	@Query(value = "SELECT *\r\n"
+			+ "     FROM (SELECT rownum r, a.*\r\n"
+			+ "           FROM (SELECT *\r\n"
+			+ "                 FROM qna_board\r\n"
+			+ "                 WHERE user_nickname LIKE'%?1%'\r\n"
+			+ "                 ORDER BY qna_board_dt DESC;\r\n"
+			+ "		            )a\r\n"
+			+ "     WHERE r BETWEEN ?2 AND ?3"
+			, nativeQuery=true)
+	List<QnABoardEntity>findByNickname(String nickName, int startRow, int endRow);
+	
 }
--- BackQnA/src/test/java/com/golflearn/domain/repository/QnABoardRepositoryTest.java
+++ BackQnA/src/test/java/com/golflearn/domain/repository/QnABoardRepositoryTest.java
@@ -3,44 +3,115 @@ package com.golflearn.domain.repository;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.golflearn.domain.entity.QnABoard;
-import com.golflearn.domain.entity.QnAComment;
+import com.golflearn.domain.entity.QnABoardEntity;
+import com.golflearn.domain.entity.QnACommentEntity;
+import com.golflearn.dto.QnABoardDto;
 @SpringBootTest
 class QnABoardRepositoryTest {
 
 	@Autowired
-	private QnABoardRepository bRepo;
+	private QnABoardRepository boardRepo;
 	@Autowired
-	private QnACommentRepository cRepo;
+	private QnACommentRepository commentRepo;
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@Test
+	@Transactional
+	//글 등록
 	void testAddBoard() {
-		QnABoard b = new QnABoard();
-		b.setBoardTitle("b_title1");
+		QnABoardEntity b = new QnABoardEntity();
+		b.setBoardNo(26L);
+		b.setBoardTitle("문의사항 등록 테스트");
 		b.setBoardContent("b_content1");
 		b.setQnaBoardDt(new Date(System.currentTimeMillis()));
+		b.setUserNickName("가나다");
 		b.setQnaBoardSecret(1);
-		bRepo.save(b);
+		boardRepo.save(b);
 	}
+	
 	@Test
+	@Transactional
+	//댓글 등록
 	void testAddComment() {
-		Optional<QnABoard> optB = bRepo.findById(2L);
+		Optional<QnABoardEntity> optB = boardRepo.findById(1L);
 		assertTrue(optB.isPresent());
+		Optional<QnACommentEntity> optB2 = commentRepo.findById(1L);
+		assertTrue(optB2.isPresent());
 		
-		QnABoard b = optB.get();
-		QnAComment c = new QnAComment();
+		QnABoardEntity b = optB.get();
+		QnACommentEntity c = new QnACommentEntity();
 		c.setBoard(b);
-		c.setCNo(b.getBNo());
+		c.setCommentNo(b.getBoardNo());
 		c.setQnaCmtContent("c_content1");
 		c.setUserNickname("c_nickname1");
 		c.setQnaCmtDt(new Date(System.currentTimeMillis()));
-		cRepo.save(c);
-		
+		commentRepo.save(c);
 	}
+	
+	@Test
+	//게시판 목록 조회
+	void testFindByPage() {
+		int currentPage = 1;
+		int cntPerPage = 3;
+		int endRow = currentPage * cntPerPage;
+		int startRow = endRow - cntPerPage + 1;
+		List<QnABoardEntity> list = boardRepo.findByPage(startRow, endRow);
+		list.forEach((b)->{
+			logger.error(b.toString());
+		}); 
+	}
+		
+	@Test
+	//글 상세보기
+	void testFindDetail() {
+		Long qnaBoardNo = 1L;
+		List<QnABoardEntity> list = boardRepo.findDetail(qnaBoardNo);
+		list.forEach((b)->{
+			logger.error(b.toString());
+		});
+	}
+	
+	@Test
+	//공개글 보기
+	void testFindByOpenPost() {
+		int currentPage = 1;
+		int cntPerPage = 3;
+		int endRow = currentPage * cntPerPage;
+		int startRow = endRow - cntPerPage + 1;
+		List<QnABoardEntity> list = boardRepo.findByOpenPost(startRow, endRow);
+		list.forEach((b)->{
+			logger.error(b.toString());
+		});
 
+	}
+	
+	@Test
+	@Transactional
+	//글 수정하기
+	void testModifyBoard() {
+		QnABoardEntity qboard = new QnABoardEntity();
+		qboard.setBoardNo(3L);
+		qboard.setBoardTitle("글 수정");
+		qboard.setBoardContent("수정");
+		boardRepo.save(qboard);
+	}
+	
+	@Test
+	@Transactional
+	//글 삭제하기
+	void testDeleteById() {
+		Long boardNo = 3L;
+		boardRepo.deleteById(boardNo);
+	}
 }
