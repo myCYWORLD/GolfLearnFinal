79c9d13e2dae19bcdd5758207ead543f9d0dd0d5
yeon <choyeon960325@gmail.com>
2022-09-06 10:21:21 +0900

    [수정] Service 코드 수정

--- BackQnA/src/main/java/com/golflearn/service/QnABoardService.java
+++ BackQnA/src/main/java/com/golflearn/service/QnABoardService.java
@@ -1,17 +1,22 @@
 package com.golflearn.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.golflearn.domain.entity.QnABoardEntity;
+import com.golflearn.domain.entity.QnACommentEntity;
 import com.golflearn.domain.repository.QnABoardRepository;
 import com.golflearn.domain.repository.QnACommentRepository;
 import com.golflearn.dto.PageBean;
 import com.golflearn.dto.QnABoardDto;
+import com.golflearn.dto.QnACommentDto;
+import com.golflearn.exception.AddException;
 import com.golflearn.exception.FindException;
+import com.golflearn.exception.ModifyException;
 
 import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
@@ -20,6 +25,10 @@ public class QnABoardService {
 	private static final int CNT_PER_PAGE = 5; //페이지별 보여줄 목록수
 	@Autowired
 	private QnABoardRepository boardRepo;
+
+	//	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Autowired
 	private QnACommentRepository commentRepo;
 	/**
 	 * 페이지별 게시글 목록과 페이지 그룹 정보를 반환한다.
@@ -103,10 +112,10 @@ public class QnABoardService {
 		PageBean<QnABoardDto> pb = new PageBean<>(dtolist, totalCnt, currentPage, cntPerPageGroup, CNT_PER_PAGE);
 		return pb;
 	}
-	
+
 	/**
 	 * 답변 대기인 글들만 목록으로 불러온다. 
-	 * @param currentPage
+	 * @param currentPage 현재 페이지
 	 * @return
 	 * @throws FindException
 	 */
@@ -131,43 +140,115 @@ public class QnABoardService {
 		PageBean<QnABoardDto> pb = new PageBean<>(dtolist, totalCnt, currentPage, cntPerPageGroup, CNT_PER_PAGE);
 		return pb;
 	}
-	
-	
-	
-	//	public QnABoardDto viewBoard(Long boardNo) throws FindException {
-	//		Optional<QnABoardEntity> boardEntity = boardRepo.findById(boardNo);
-	//		if(boardEntity.isPresent()) {
-	//			QnABoardEntity board = boardEntity.get();
-	//			//		System.out.println(board);
-	//			QnABoardDto boardDto = QnABoardDto.builder()
-	//							.boardNo(board.getBoardNo())
-	//							.boardTitle(board.getBoardTitle())
-	//							.boardContent(board.getBoardContent())
-	//							.userNickname(board.getUserNickname())
-	//							.qnaBoardDt(board.getQnaBoardDt())
-	//							.build();
-	//			return boardDto;
-	//		}else {
-	//			throw new FindException("게시글이 없습니다");
-	//		}
-	//	}
+
+	/**
+	 * 게시글 상세보기
+	 * @param boardNo 게시글 번호
+	 * @return
+	 * @throws FindException
+	 */
+	public QnABoardDto viewBoard(Long boardNo) throws FindException {
+		Optional<QnABoardEntity> boardEntity = boardRepo.findById(boardNo);
+		if(boardEntity.isPresent()) {
+			QnABoardEntity board = boardEntity.get();
+			QnABoardDto boardDto = QnABoardDto.builder()
+					.boardNo(board.getBoardNo())
+					.boardTitle(board.getBoardTitle())
+					.boardContent(board.getBoardContent())
+					.userNickname(board.getUserNickname())
+					.qnaBoardDt(board.getQnaBoardDt())
+					.qnaBoardSecret(board.getQnaBoardSecret())
+					.comment(board.getComment())
+					.build();
+			return boardDto;
+		}else {
+			throw new FindException("게시글이 없습니다");
+		}
+	}
+
+	/**
+	 * 글을 작성한다
+	 * @param qnaBoard 
+	 * @throws AddException
+	 */
+	public void writeBoard(QnABoardDto qnaBoard) throws AddException {
+		//사용자가 쓴 게시글을 Dto -> entity로 변환 시켜서 저장
+		QnABoardEntity be = qnaBoard.toEntity();
+		boardRepo.save(be);
+	}
+
+	/**
+	 * 답변을 작성한다.
+	 * @param qnaComment
+	 * @throws AddException
+	 */
+	public void writeComment(QnACommentDto qnaComment)throws AddException {		
+		QnACommentEntity ce  = qnaComment.toEntity();
+		Optional<QnABoardEntity> optB = boardRepo.findById(qnaComment.getCommentNo());
+		System.out.println(qnaComment.getCommentNo());
+		//		logger.error("no"+qnaComment.getCommentNo());
+		if (optB.isPresent()) {
+			QnABoardEntity b = optB.get();			
+			ce.setBoard(b);
+			commentRepo.save(ce);
+		}else {
+			throw new AddException("잘못된 요청입니다.");
+		}
+	}
+
+	/**
+	 * 게시글을 수정한다
+	 * @param boardNo
+	 */
+	public void modifyBoard(QnABoardDto qnaBoard) throws ModifyException {
+		Optional<QnABoardEntity> optB = boardRepo.findById(qnaBoard.getBoardNo());
+		if (optB.isPresent()) {
+			//db에서 받아온값(optB.get)을 QnABoardEntity 타입의 qe에 담아줌 
+			QnABoardEntity qe = optB.get();
+			if(qe.getComment() == null && qnaBoard.getUserNickname().equals(qe.getUserNickname())) {
+				qe = QnABoardEntity.builder()
+						.boardNo(qe.getBoardNo())
+						.boardContent(qnaBoard.getBoardContent())
+						.boardTitle(qnaBoard.getBoardTitle())
+						.userNickname(qe.getUserNickname())
+						.qnaBoardDt(qe.getQnaBoardDt())
+						.qnaBoardSecret(qnaBoard.getQnaBoardSecret())
+						.build();
+				boardRepo.save(qe);
+			}else {
+				throw new ModifyException("답변이 달린 글은 수정할 수 없습니다");
+			}
+		}
+		//			QnABoardDto board = QnABoardDto.builder()
+		//					.boardNo(qe.getBoardNo())
+		//					.boardTitle(qnaBoard.getBoardTitle())
+		//					.boardContent(qnaBoard.getBoardContent())
+		//					.userNickname(qe.getUserNickname())
+		//					.qnaBoardDt(qe.getQnaBoardDt())
+		//					.qnaBoardSecret(qnaBoard.getQnaBoardSecret())
+		//					.build();
+		//			board.toEntity();
+	}
+
+	/**
+	 * 게시글을 삭제한다
+	 * @param boardNo
+	 */
+	public void deleteBoard(Long boardNo) {
+
+
+	}
+
+	/**
+	 * 답변을 삭제한다(관리자만 가능)
+	 * @param commentNo
+	 */
+	public void deleteComment(Long commentNo) {
+
+	}
+
 }
-//		List<QnABoardEntity> board = boardRepo.findDetail(boardNo);
-//		
-//		
-//		List<QnABoardDto> viewboard = board.stream() 
-//						.map(t -> QnABoardDto.builder()
-//						.boardNo(t.getBoardNo())
-//						.boardTitle(t.getBoardTitle())
-//						.userNickname(t.getUserNickname())
-//						.qnaBoardDt(t.getQnaBoardDt())
-//						.comment(t.getComment())
-//						.build())
-//						.collect(Collectors.toList());
-//		
-//		ResultBean<QnABoardDto> rb = new ResultBean<>(viewboard);
-//		
-//		return rb;
-//	}
+
+
 
 
